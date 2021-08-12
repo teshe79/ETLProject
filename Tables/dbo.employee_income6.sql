b10d01865669148214b65adbf5b032a2cd178d87
teshe79 <lemmaandt@gmail.com>
2021-08-12 12:29:00 -0700

    i edited the table

---
+++ Tables/dbo.employee_income6.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[employee_income6] (
+  [ID] [float] NULL,
+  [Department] [nvarchar](255) NULL,
+  [LastName] [nvarchar](255) NULL,
+  [Age] [float] NULL,
+  [Income] [float] NULL,
+  [FamilySize] [float] NULL,
+  [Address] [varchar](30) NULL,
+  [City] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
+
+CREATE CLUSTERED INDEX [teshe_ClusteredIndex]
+  ON [dbo].[employee_income6] ([ID])
+  ON [PRIMARY]
+GO
