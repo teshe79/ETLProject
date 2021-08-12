1a4df0f461568193aa829585d29e8da44c1c2ad7
teshe79 <lemmaandt@gmail.com>
2021-08-12 07:34:28 -0700

    i edit this table

---
+++ Tables/dbo.Audit.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Audit] (
+  [ID] [int] NOT NULL,
+  [Department] [varchar](25) NOT NULL,
+  [LastName] [varchar](25) NOT NULL,
+  [Age] [int] NOT NULL,
+  [Income] [bigint] NULL,
+  [FamilySize] [int] NULL,
+  [ExecutionStartTime] [nvarchar](100) NULL,
+  [MachinName] [nvarchar](100) NULL,
+  [UserName] [nvarchar](100) NULL,
+  [Package ID] [nvarchar](100) NULL,
+  [TaskID] [nvarchar](100) NULL
+)
+ON [PRIMARY]
+GO
